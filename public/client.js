a1bf736ccde74a505b182333cde9070121671bc7
Christian Martineau <cmartineau0616@gmail.com>
2025-04-14 17:43:01 -0600

    Speaks messages when received or clicked

--- public/client.js
+++ public/client.js
@@ -63,18 +63,19 @@ function submitText() {
     socket.send(JSON.stringify(message));
 }
 
-function speakMessage(text){
+function speakMessage(text) {
     const utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = speech.getVoices()[1];
     speech.speak(utterance);
 }
 
-function record(){
+function record() {
     recognition.lang = document.getElementById("languageSelect").value;
     recognition.start();
     console.log('Ready to receive your words.');
 }
 
-recognition.onresult = async function(event) {
+recognition.onresult = async function (event) {
     //show a spinner while we listen
 
     //Send the text to the server to process
@@ -82,26 +83,31 @@ recognition.onresult = async function(event) {
     document.getElementById("messageInput").value = text;
 }
 
-recognition.onspeechend = function() {
+recognition.onspeechend = function () {
     console.log("stopping recording.");
     recognition.stop();
 }
 
-recognition.onerror = function(event) {
-  console.error("Something went wrong...: "+event.error)
+recognition.onerror = function (event) {
+    console.error("Something went wrong...: " + event.error)
 }
 
-async function switchLanguage(){
-    const selectedLang = document.getElementById("languageSelect").value;
-    const recordBtn = document.getElementById("recordBtn");
-    const sendBtn = document.getElementById("sendBtn");
-    await refreshMessages();
-    if (selectedLang == "en"){
-        recordBtn.innerHTML = "🎤 Record";
-        sendBtn.innerHTML = "Send";
-    }else{
-        recordBtn.innerHTML = "🎤 Recordar";
-        sendBtn.innerHTML = "Enviar";
+async function switchLanguage() {
+    try {
+        const selectedLang = document.getElementById("languageSelect").value;
+        const recordBtn = document.getElementById("recordBtn");
+        const sendBtn = document.getElementById("sendBtn");
+        document.documentElement.lang = selectedLang;
+        await refreshMessages();
+        if (selectedLang == "en") {
+            recordBtn.innerHTML = "🎤 Record";
+            sendBtn.innerHTML = "Send";
+        } else {
+            recordBtn.innerHTML = "🎤 Recordar";
+            sendBtn.innerHTML = "Enviar";
+        }
+    } catch (e) {
+        console.error(e);
     }
 }
 
@@ -112,7 +118,7 @@ async function refreshMessages() {
     renderMessages(data.messages);
 }
 
-function renderMessages(messages){
+function renderMessages(messages) {
     const messageContainer = document.getElementById("messageContainer");
     messageContainer.innerHTML = "";
     messages.forEach(message => {
@@ -123,14 +129,14 @@ function renderMessages(messages){
 function renderMessage(message) {
     const messageContainer = document.getElementById("messageContainer");
     const selectedLang = document.getElementById("languageSelect").value;
-    
+
     const messageDiv = document.createElement("div");
     messageDiv.classList.add("d-flex", "mb-2");
 
     const messageBubble = document.createElement("div");
     messageBubble.classList.add("p-2", "rounded", "text-white");
     messageBubble.style.maxWidth = "75%";
-    
+
     if (message.clientId === clientId) {
         messageDiv.classList.add("justify-content-end");
         messageBubble.classList.add("bg-primary");
@@ -138,19 +144,27 @@ function renderMessage(message) {
         messageDiv.classList.add("justify-content-start");
         messageBubble.classList.add("bg-secondary");
     }
-    
+
+    messageDiv.style.cursor = "pointer";
+
     const messageText = document.createElement("div");
     messageText.innerHTML = `${message[selectedLang]}`;
     messageBubble.appendChild(messageText);
-    
+
     const timestamp = document.createElement("div");
     timestamp.classList.add("text-start", "text-light", "mt-2");
     timestamp.style.fontSize = "0.75rem";
     timestamp.textContent = new Date(message.datetime).toLocaleString();
     messageBubble.appendChild(timestamp);
-    
+
     messageDiv.appendChild(messageBubble);
+    
+    messageDiv.addEventListener("click", (e)=>{
+        speakMessage(message[selectedLang]);
+    })
+    
     messageContainer.appendChild(messageDiv);
 
     messageContainer.scrollTop = messageContainer.scrollHeight;
+    
 }
